7edce67053231ae4a5612be217fef5d695e60b16
Dr. Dillinger <109082465+phd-srh@users.noreply.github.com>
2024-03-21 10:47:45 +0100

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+ /*
+     * Ein Spiel für 2 Spieler.
+     * Jeder Spieler kann 1-10 Steine auf einem Spielbrett plazieren,
+     * selbst gewählt, mindestens eins, maximal 10 Steine.
+     * Welcher Spieler den 100. Stein hinlegt, hat gewonnen.
+     * Das Spiel beginnt mit einer zufälligen Anzahl von Steinen (10 - 30),
+     * die bereits auf dem Spielbrett liegen.
+     *
+     * Benutzen Sie das Konzept der Vererbung, um wahlweise einen
+     * menschlichen oder computergesteuerten Spieler teilnehmen zu lassen
+     * (0, 1 oder 2 Computerspieler).
+     *
+     * Verwenden Sie am Anfang eine schwache KI für den computergesteuerten
+     * Spieler.
+     *
+     * Notwendige Klassen:
+     * - Spieler
+     *   - MenschSpieler
+     *   - ComputerSpieler
+     * - Spielbrett
+     */
+  
+}
